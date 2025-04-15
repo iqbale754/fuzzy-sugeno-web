8b8de197aa7d23449aff41566dbb3ccd0f5a1e92
iqbale754 <firdausiqbal754@gmail.com>
2025-04-15 13:45:05 +0800

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import FuzzySugenoApp from "../components/FuzzySugenoApp";
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50 py-10 px-4">
+      <FuzzySugenoApp />
+    </main>
+  );
+}
